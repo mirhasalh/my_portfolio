d89b3a8c9ce3ea6b379a0d567f96b87505c2e9c4
Irhas <muhammadirhasalh@gmail.com>
2022-09-13 21:47:12 +0700

    Update web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "60ed5a3034405e6ba5bbf45325dd8d39",
-"/": "60ed5a3034405e6ba5bbf45325dd8d39",
-"main.dart.js": "beb7e7171d45cd45cd29f067eab3ad81",
+"index.html": "dad0fa170d4a4b6eeaa9f0375f5005d8",
+"/": "dad0fa170d4a4b6eeaa9f0375f5005d8",
+"main.dart.js": "491ba22a6ece391b09ec06f9f2aba446",
 "manifest.json": "d8fe34f7ae4c072a77b924e01dac8a50",
 "version.json": "9b818ca9511483c901bed1545384376c"
 };

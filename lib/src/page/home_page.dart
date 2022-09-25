50488a1b7db0c1026f7c1b5d70a2e685947ab55f
Irhas <muhammadirhasalh@gmail.com>
2022-09-25 21:40:54 +0700

    Implement staggered grid

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:my_portfolio/src/model/projects_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,16 +13,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final Future<List<ProjectsModel>> _projects = _getProjects();
+
+  @override
+  void initState() {
+    super.initState();
+    _getProjects();
+  }
+
+  static const String _whitespaces = "    ";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Folio'),
       ),
-      body: Center(
-        child: Image.network(
-            'https://images.unsplash.com/photo-1661961112134-fbce0fdf3d99?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'),
+      body: ListView(
+        children: [
+          FutureBuilder<List<ProjectsModel>>(
+            future: _projects,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return const Center(child: Text("Error"));
+              } else {
+                final data = snapshot.data;
+                return StaggeredGrid.count(
+                  crossAxisCount:
+                      MediaQuery.of(context).size.width <= 500 ? 2 : 4,
+                  mainAxisSpacing: 4.0,
+                  crossAxisSpacing: 4.0,
+                  children: List.generate(
+                    data!.length,
+                    (index) => Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(data[index].image!),
+                          const SizedBox(height: 12.0),
+                          Text("$_whitespaces${data[index].title!}"),
+                          Text("${_whitespaces}Start: ${data[index].est!}"),
+                          Text("${_whitespaces}Until: ${data[index].until!}"),
+                          const SizedBox(height: 12.0),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+        ],
       ),
     );
+  }
+
+  Future<List<ProjectsModel>> _getProjects() async {
+    final response = await rootBundle.loadString("json/projects.json");
+
+    final data = json.decode(response);
+
+    return (data as List).map((e) => ProjectsModel.fromJson(e)).toList();
   }
 }

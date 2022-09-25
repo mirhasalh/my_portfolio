6433411d8323fc5076e36be0cba444725a7354e8
Irhas <muhammadirhasalh@gmail.com>
2022-09-25 16:11:50 +0700

    Add responsive_framework 0.2.0

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -10,10 +10,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello world!'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Folio'),
+        ),
+        body: Wrap(
+          children: [
+            Container(
+              color: Colors.red,
+              height: 100.0,
+              width: 100.0,
+            ),
+            Container(
+              color: Colors.purple,
+              height: 100.0,
+              width: 300.0,
+            ),
+            Container(
+              color: Colors.green,
+              height: 100.0,
+              width: 100.0,
+            ),
+          ],
+        ));
   }
 }

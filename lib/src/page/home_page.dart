b36f781459f3f99a1085b095adc28c3371ac3559
Irhas <muhammadirhasalh@gmail.com>
2022-09-25 16:23:55 +0700

    Update android label

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -11,27 +11,12 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Folio'),
-        ),
-        body: Wrap(
-          children: [
-            Container(
-              color: Colors.red,
-              height: 100.0,
-              width: 100.0,
-            ),
-            Container(
-              color: Colors.purple,
-              height: 100.0,
-              width: 300.0,
-            ),
-            Container(
-              color: Colors.green,
-              height: 100.0,
-              width: 100.0,
-            ),
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text('Flutter Folio'),
+      ),
+      body: const Center(
+        child: Text('No content'),
+      ),
+    );
   }
 }

d41eb038fad6b356ba8bc9ddf2c88577cd0eebb5
Irhas <muhammadirhasalh@gmail.com>
2022-09-26 17:21:44 +0700

    Remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:my_portfolio/my_portfolio.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 void main() {
-  // TODO: Set url strategy to remove the leading hash symbol
   setPathUrlStrategy();
   runApp(const MyPortfolio());
 }

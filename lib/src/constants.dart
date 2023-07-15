edce247edf8a35f96ac381c04c29a80b7dba16bb
Irhas <muhammadirhasalh@gmail.com>
2023-07-16 05:46:35 +0700

    Update kDropboxUrl

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -2,4 +2,4 @@ const kGitHubIcon = 'svg/github.svg';
 const kGitRepoUrl = 'https://github.com/mirhasalh/my_portfolio';
 const kImgBbBaseUrl = 'https://i.ibb.co';
 const kDropboxUrl =
-    'https://www.dropbox.com/sh/6c9e5v8pk1wx5r4/AACxASmgj5xSzi5yHhZy0GEZa?dl=0';
+    'https://www.dropbox.com/scl/fo/cqoghbdsa3iisbusoxkkm/h?rlkey=wz8cyejx1auq8zo3f0tpl0mgu&dl=0';
